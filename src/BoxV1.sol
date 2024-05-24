1e20a5cac2605323d8a5f48defb201571b47619f
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-05-24 19:12:17 +0200

    progress with proxy contract calls using openzeppelin

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -4,15 +4,28 @@ pragma solidity ^0.8.18;
 
 // import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 // import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
-// import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-// import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 
-contract BoxV1 is UUPSUpgradeable {
+// storage is stored in the proxy, not in the implementation
+// Proxy (borrowing implementation funcs) --> implementation
+/* Proxy (number = 0)   --> implementation (number = 1) */
+
+contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
 
-    error BoxV1__OwnerIsNotMsgSender();
+    // @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    /* constructor for proxys */
+    function initialize() public initializer {
+        __Ownable_init(msg.sender); // sets owner to: owner = msg.sender
+        __UUPSUpgradeable_init();
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -2,7 +2,11 @@
 
 pragma solidity ^0.8.18;
 
-contract BoxV2 {
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract BoxV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
 
     function setNumber(uint256 _number) external {}
@@ -13,5 +17,9 @@ contract BoxV2 {
 
     function version() external pure returns (uint256) {
         return 2;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override {
+        // if (owner != msg.sender) revert BoxV1__OwnerIsNotMsgSender();
     }
 }
