4834343068148048cac8009e1ce087d11bb35794
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-05-24 18:55:07 +0200

    progress with adding uups functions

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -2,8 +2,17 @@
 
 pragma solidity ^0.8.18;
 
-contract BoxV1 {
+// import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+// import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+// import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+// import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+
+contract BoxV1 is UUPSUpgradeable {
     uint256 internal number;
+
+    error BoxV1__OwnerIsNotMsgSender();
 
     function getNumber() external view returns (uint256) {
         return number;
@@ -12,4 +21,9 @@ contract BoxV1 {
     function version() external pure returns (uint256) {
         return 1;
     }
+
+    function _authorizeUpgrade(address newImplementation) internal override {
+        // if (owner != msg.sender) revert BoxV1__OwnerIsNotMsgSender();
+    }
+
 }
---
+++ src/BoxV2.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract BoxV2 {
+    uint256 internal number;
+
+    function setNumber(uint256 _number) external {}
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    function version() external pure returns (uint256) {
+        return 2;
+    }
+}
